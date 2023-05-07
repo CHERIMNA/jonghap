e22369d502bbb152fed32280c4378346fd29ba0f
taehee129 <89380104+taehee129@users.noreply.github.com>
2023-05-07 20:27:40 +0900

    Delete Model directory

--- Model/TranslateInfo.py
+++
@@ -1,8 +0,0 @@
-class TranslateInfo: 
-    def __init__(self, api , text , sourceLang, targetLang):
-        self.api = api 
-        self.text = text 
-        self.sourceLang = sourceLang 
-        self.targetLang = targetLang
-   
-    
